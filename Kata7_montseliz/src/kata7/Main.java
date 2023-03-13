aa6ca351e099e098cdaa4521e13931aab89d0e44
montseliz <montse.liz92@gmail.com>
2023-03-13 12:32:57 +0100

    incloure opcio numeros

--- Kata7_montseliz/src/kata7/Main.java
+++ Kata7_montseliz/src/kata7/Main.java
@@ -12,6 +12,7 @@ public class Main {
         int passwordLength = input.nextInt();
 
         System.out.println("El teu password generat aleatòriament és: " + generatePassword(passwordLength));
+        System.out.println("\nEl teu password generat aleatòriament és: " + generatePassword2(passwordLength));
 
     }
 
@@ -19,17 +20,20 @@ public class Main {
         String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String lowerCase = "abcdefghijklmnopqrstuvwxyz";
         String symbols = "!@#$%^&*(){}[]=<>";
+        String numbers = "0123456789";
         String password = "";
         Random random = new Random();
 
         for (int i = 0; i < passwordLength; i++) {
-            int type = random.nextInt(3);
+            int type = random.nextInt(4);
             if (type == 0) { //Majúscula
                 password += upperCase.charAt(random.nextInt(upperCase.length()));
             } else if (type == 1) { //Minúscula
                 password += lowerCase.charAt(random.nextInt(lowerCase.length()));
-            } else { //Símbol
+            } else if (type == 2){ //Símbol
                 password += symbols.charAt(random.nextInt(symbols.length()));
+            } else { //Números
+                password += numbers.charAt(random.nextInt(numbers.length()));
             }
         }
 
@@ -39,8 +43,9 @@ public class Main {
     /**
      * Per generar la majúscula o la minúscula aleatòria, s'utilitza la funció random.nextInt(26),
      * que retorna un número aleatori entre 0 i 25 (ja que hi ha 26 lletres a l'alfabet).
-     * Aquest número aleatori es suma a 'A' (per a majúscules) o a 'a' (per a minúscules)
-     * per obtenir el codi ASCII de la lletra corresponent.
+     * En el cas dels números s'utilitza random.nextInt(10) perquè són 10 els caràcters numèrics possibles.
+     * Aquest número aleatori es suma a 'A' (per a majúscules), a 'a' (per a minúscules), o a '0' per números
+     * per obtenir el codi ASCII corresponent.
      */
     public static String generatePassword2(int passwordLength) {
         String symbols = "!@#$%^&*(){}[]=<>";
@@ -48,13 +53,15 @@ public class Main {
         Random random = new Random();
 
         for (int i = 0; i < passwordLength; i++) {
-            int type = random.nextInt(3);
+            int type = random.nextInt(4);
             if (type == 0) { //Majúscula
                 password += (char) (random.nextInt(26) + 'A');
             } else if (type == 1) { //Minúscula
                 password += (char) (random.nextInt(26) + 'a');
-            } else { //Símbol
+            } else if (type == 2){ //Símbol
                 password += symbols.charAt(random.nextInt(symbols.length()));
+            } else { //Números
+                password += (char) (random.nextInt(10) + '0');
             }
         }
 
--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 
 	1.- Demanar-li una longitud de password a l'usuari.
 
-	2.- Retornar-li un password generat aleatòriament amb majúscules, minúscules i els següents símbols: !,@,#,$,%,^,&,*,(,),{,},[,],=,<,>
+	2.- Retornar-li un password generat aleatòriament amb majúscules, minúscules, números i els següents símbols: !,@,#,$,%,^,&,*,(,),{,},[,],=,<,>
 
-	3.- No es defineix la quantitat de majúscules, minúscules i símbols requerits per cada password. Per tant, pot ser que no hi hagi algun dels tipus indicats.
+	3.- No es defineix la quantitat de majúscules, minúscules, números i símbols requerits per cada password. Per tant, pot ser que no hi hagi algun dels tipus indicats.
 
 	
